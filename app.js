121fa9f7d076796bef21dd3eff02df7f55773612
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-10-17 20:58:31 -0400

    Directives, data and functions

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const app = Vue.createApp({
     // data, functions to react to events that we want
     //template: '<h2>I am the template</h2>'
+
+    // Make a function & then return object within that function
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    }
 })
 
 app.mount('#app')
