1a1fd501c651b25fce900a0b32e8124416c74623
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-10-18 13:06:18 -0400

    Data/attribute binding

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const app = Vue.createApp({
     // Make a function & then return object within that function
     data() {
         return {
+            url: 'http://www.thenetninja.co.uk',
             showBooks: true,
             books: [
-                { title: 'name of the wind', author: 'patrick rothfuss'},
-                { title: 'the way of kings', author: 'brandon sanderson'},
-                { title: 'the final empire', author: 'brandon sanderson'}
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg'},
+                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg'},
+                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg'}
             ],
             x: 0,
             y: 0
